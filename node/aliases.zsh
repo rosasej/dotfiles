fa1fd44ca01ef3992f59755f7aaeeb16b102fe4a
Donny West <donnywest@gmail.com>
2017-02-22 08:29:53 -0500

    npm aliases

--- node/aliases.zsh
+++ node/aliases.zsh
@@ -4,30 +4,4 @@ alias npisd='npm install --save-dev'
 alias npig='npm install -g'
 alias npit='npm init'
 alias npi='npm install'
-
-# #
-# # Loads the Node Version Manager and enables npm completion.
-# #
-# # Authors:
-# #   Sorin Ionescu <sorin.ionescu@gmail.com>
-# #   Zeh Rizzatti <zehrizzatti@gmail.com>
-# #
-
-# # Load manually installed NVM into the shell session.
-# if [[ -s "$HOME/.nvm/nvm.sh" ]]; then
-#   source "$HOME/.nvm/nvm.sh"
-# fi
-
-# # Load NPM completion.
-# if (( $+commands[npm] )); then
-#   cache_file="${0:h}/cache.zsh"
-
-#   if [[ "$commands[npm]" -nt "$cache_file" || ! -s "$cache_file" ]]; then
-#     # npm is slow; cache its output.
-#     npm completion >! "$cache_file" 2> /dev/null
-#   fi
-
-#   source "$cache_file"
-
-#   unset cache_file
-# fi
+alias nplg='npm ls -g --depth=0'
