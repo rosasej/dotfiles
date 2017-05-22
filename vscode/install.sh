b6ea3758fa212a8b1161854ef806a60a44452178
Donny West <donnywest@gmail.com>
2017-05-22 09:07:01 -0400

    tweak vscode symlinks

--- vscode/install.sh
+++ vscode/install.sh
@@ -6,8 +6,8 @@ if test "$(which code)"; then
     VSCODE_HOME="$HOME/.config/Code"
   fi
 
-  ln -sf "$DOTFILES/vscode/settings.json" "$VSCODE_HOME/User/settings.json"
-  ln -sf "$DOTFILES/vscode/keybindings.json" "$VSCODE_HOME/User/keybindings.json"
+  ln -s "$DOTFILES/vscode/settings.json" "$VSCODE_HOME/User/settings.json"
+  ln -s "$DOTFILES/vscode/keybindings.json" "$VSCODE_HOME/User/keybindings.json"
 
   # from `code --list-extensions`
   modules="
