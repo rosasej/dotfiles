dbe133bef53bececdc33abbe148a94868e2fbc42
Donny West <donnywest@gmail.com>
2017-05-22 10:38:15 -0400

    try fork of pure prompt with node version

--- antibody/install.sh
+++ antibody/install.sh
@@ -3,5 +3,6 @@ if [ "$(uname -s)" != "Darwin" ]; then
   exit 0
 fi
 antibody bundle < "$ZSH/antibody/pre_prompt.txt" > ~/.antibody_bundles.txt
-antibody bundle sindresorhus/pure >> ~/.antibody_bundles.txt
+# antibody bundle sindresorhus/pure >> ~/.antibody_bundles.txt
+antibody bundle dwest-teo/pure >> ~/.antibody_bundles.txt
 antibody bundle < "$ZSH/antibody/post_prompt.txt" >> ~/.antibody_bundles.txt
