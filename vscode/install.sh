036129ba32e7ce616f5f4dbc1fa0d046389bd2a7
Donny West <donnywest@gmail.com>
2017-05-22 09:22:18 -0400

    try reverting to indiv setting files for vscode

--- vscode/install.sh
+++ vscode/install.sh
@@ -6,7 +6,8 @@ if test "$(which code)"; then
     VSCODE_HOME="$HOME/.config/Code"
   fi
 
-  ln -sf "$DOTFILES/vscode/User" "$VSCODE_HOME/User"
+  ln -sf "$DOTFILES/vscode/settings.json" "$VSCODE_HOME/User/settings.json"
+  ln -sf "$DOTFILES/vscode/keybindings.json" "$VSCODE_HOME/User/keybindings.json"
 
   # from `code --list-extensions`
   modules="
