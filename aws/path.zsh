ad6cd55eb0b6443655c0ec5b2451917ce64c7106
dwest-teo <donnywest@gmail.com>
2017-08-25 13:31:33 -0400

    add swapex parallelism

--- aws/path.zsh
+++ aws/path.zsh
@@ -4,3 +4,4 @@ export AWS_ACCESS_KEY_ID=$(aws configure get aws_access_key_id)
 export AWS_SECRET_ACCESS_KEY=$(aws configure get aws_secret_access_key)
 export AWS_DEFAULT_REGION=$(aws configure get region)
 export SWAPEX_ENVIRONMENT=dwest
+export SWAPEX_PARALLELISM=100
