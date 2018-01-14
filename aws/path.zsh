aacdb3bd91b73e80c7af98a683d25de784bd8d2d
Javier Rosas <javier@Javiers-Mac-mini.local>
2018-01-14 14:37:20 -0500

    update path, remove aws

--- aws/path.zsh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-export AWS_DEFAULT_PROFILE=dwest
-export AWS_ACCESS_KEY_ID=$(aws configure get aws_access_key_id)
-export AWS_SECRET_ACCESS_KEY=$(aws configure get aws_secret_access_key)
-export AWS_DEFAULT_REGION=$(aws configure get region)
-export SWAPEX_ENVIRONMENT=dwest
-export SWAPEX_PARALLELISM=100
